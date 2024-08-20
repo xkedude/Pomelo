4ed74a6432e63f448d93c20753ca8ebbc235f50d
Catsoftware <leopold.schaffhauser@gmail.com>
2024-08-20 11:19:30 +0100

    OG Pomelo is back

---
+++ Pomelo/LibraryViews/GameButton/GameButtonListView.swift
@@ -0,0 +1,43 @@
+//
+//  GameButtonListView.swift
+//  Pomelo
+//
+//  Created by TechGuy on 20/8/24.
+//  Copyright © 2024 TechGuy. All rights reserved.
+//
+
+import SwiftUI
+import Foundation
+import UIKit
+
+struct GameButtonListView: View {
+    var game: PomeloGame
+
+    var body: some View {
+        HStack(spacing: 15) {
+            if let image = UIImage(data: game.imageData) {
+                Image(uiImage: image)
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .cornerRadius(8)             } else {
+                Image(systemName: "photo")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .cornerRadius(8)
+            }
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(game.title)
+                    .font(.headline)
+                    .foregroundColor(.blue)
+                
+                Text(game.developer)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
+        }
+        .padding(.vertical, 8) 
+    }
+}
--- Pomelo/LibraryViews/GameList/GameListView.swift
+++ Pomelo/LibraryViews/GameList/GameListView.swift
@@ -14,40 +14,71 @@ struct GameListView: View {
     @State var core: Core
     @State private var searchText = ""
     @State var game: PomeloGame? = nil
+    @State private var isGridView = true
 
     var body: some View {
         let filteredGames = core.games.filter { game in
             if let PomeloGame = game as? PomeloGame {
                 return searchText.isEmpty || PomeloGame.title.localizedCaseInsensitiveContains(searchText)
             }
-            return false // Default case if the cast fails
+            return false
         }
+
+        
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                    isGridView.toggle()
+                }) {
+                    Image(systemName: isGridView ? "rectangle.grid.1x2" : "square.grid.2x2")
+                        .imageScale(.large)
+                        .padding()
+                    Spacer()
+                }
+            
+                Spacer()
+            }
+            //.padding(.top, -65)
+            //.padding(.bottom, 30)
+
         ScrollView {
             VStack(alignment: .leading) {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 160))], spacing: 10) {
-                    ForEach(0..<filteredGames.count, id: \.self) { index in
-                        if let game = core.games[index] as? PomeloGame {
-                            
-                            NavigationLink(destination: SudachiEmulationView(game: game).toolbar(.hidden, for: .tabBar)) {
-                                GameButtonView(game: game)
-                                    .frame(maxWidth: .infinity, minHeight: 200) // Set a consistent height for each row
+                
+                if isGridView {
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 160))], spacing: 10) {
+                        ForEach(0..<filteredGames.count, id: \.self) { index in
+                            if let game = core.games[index] as? PomeloGame {
+                                NavigationLink(destination: SudachiEmulationView(game: game).toolbar(.hidden, for: .tabBar)) {
+                                    GameButtonView(game: game)
+                                        .frame(maxWidth: .infinity, minHeight: 200)
+                                }
                             }
-                             
-                        
+                        }
+                    }
+                } else {
+                    LazyVStack() {
+                        ForEach(0..<filteredGames.count, id: \.self) { index in
+                            if let game = core.games[index] as? PomeloGame {
+                                NavigationLink(destination: SudachiEmulationView(game: game).toolbar(.hidden, for: .tabBar)) {
+                                    GameButtonListView(game: game)
+                                        .frame(maxWidth: .infinity, minHeight: 75)
+                                }
+                            }
                         }
                     }
                 }
-                .searchable(text: $searchText) // Add this line
-                .frame(maxWidth: .infinity)
             }
-            .padding()
-        }
-        .onAppear() {
-            refreshcore()
-        }
-        .refreshable {
-            core = Core(console: Core.Console.nSwitch, name: .Pomelo, games: [], root: FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first ?? URL(string: "/")!)
-            refreshcore()
+                .searchable(text: $searchText)
+                .padding()
+            }
+            .onAppear() {
+                refreshcore()
+            }
+            .refreshable {
+                core = Core(console: Core.Console.nSwitch, name: .Pomelo, games: [], root: FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first ?? URL(string: "/")!)
+                refreshcore()
+            }
         }
     }
     
